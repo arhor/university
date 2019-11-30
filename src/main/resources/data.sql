69abf23c9474d68bcc14a2f723d2476b3ae382e5
Arhor <DKY8pdej>
2019-11-30 12:32:31 +0300

    project structure refactored;

--- src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO roles (title) VALUES ('USER'), ('ADMIN')
-
-INSERT INTO langs (label) VALUES ('BY'), ('RU'), ('EN')
---
+++ university-server/src/main/java/by/bsu/uir/university/web/api/v1/ExceptionHandler.java
@@ -0,0 +1,7 @@
+package by.bsu.uir.university.web.api.v1;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionHandler {
+}
