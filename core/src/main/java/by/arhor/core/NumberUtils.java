6ccd8fe67c0dd31ce6ccf686301dc4f0566bd8c9
Maksim Buryshynets <Maksim_Buryshunets@epam.com>
2019-12-01 23:15:09 +0300

    java docs added to NumberUtils;

--- core/src/main/java/by/arhor/core/NumberUtils.java
+++ core/src/main/java/by/arhor/core/NumberUtils.java
@@ -6,6 +6,15 @@ public final class NumberUtils {
 
   private NumberUtils() {}
 
+  /**
+   * Method supposed to be used for creation number bounding function,
+   * which guaranties that it will return passed value only in case that
+   * it is higher than bound.
+   *
+   * @param bound min value to be returned by created function
+   * @param <N> any number type
+   * @return closure with min bound value
+   */
   public static <N extends Number> ToIntFunction<N> minBound(final int bound) {
     return (arg) ->
         ((arg == null) || (arg.intValue() <= bound))
@@ -13,6 +22,15 @@ public final class NumberUtils {
             : arg.intValue();
   }
 
+  /**
+   * Method supposed to be used for creation number bounding function,
+   * which guaranties that it will return passed value only in case that
+   * it is lower than bound.
+   *
+   * @param bound max value to be returned by created function
+   * @param <N> any number type
+   * @return closure with max bound value
+   */
   public static <N extends Number> ToIntFunction<N> maxBound(final int bound) {
     return (arg) ->
         ((arg == null) || (arg.intValue() <= 0) || (arg.intValue() >= bound))
