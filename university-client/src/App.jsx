f4d95d2e70a59353453738d3b25820181b40c259
Arhor <DKY8pdej>
2019-12-24 09:36:33 +0300

    idk commit

--- university-client/src/App.jsx
+++ university-client/src/App.jsx
@@ -2,18 +2,21 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import MainContent from './components/MainContent';
 import SideNavBar from './components/SideNavBar';
 import RouterView from './routes';
+import Row from "./components/Row";
+import Container from "./components/Container";
 
 export default function App() {
   return (
     <Router>
       <Header />
-      <MainContent>
-        <SideNavBar />
-        <RouterView />
-      </MainContent>
+      <Container>
+        <Row>
+          <SideNavBar />
+          <RouterView />
+        </Row>
+      </Container>
       <Footer />
     </Router>
   );
