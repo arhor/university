2ffcb8895a2e35cb0b7f8afed842dcba2cc75d91
Arhor <DKY8pdej>
2019-12-25 23:45:13 +0300

    langs omg added;

--- university-client/src/localization/LocalizationService.js
+++ university-client/src/localization/LocalizationService.js
@@ -1,16 +1,17 @@
 import Service from '../core/Service';
 
 class LocalizationService extends Service {
-
   async localize(label) {
     let text = localStorage.getItem(label);
 
     if (text === null || text === undefined) {
       const { data } = this.api.get('labels');
       text = data;
+      localStorage.setItem(label, text);
     }
 
     return text;
   }
-
 }
+
+export default new LocalizationService();
