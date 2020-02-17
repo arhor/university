f96a9e7e7e780f8e7cf2090dbd757e9d07d5a1eb
arhor <RZAB9qkn>
2020-02-17 08:34:53 +0300

    all inits have to be implemented

--- university-database/src/main/resources/sql/data/init-faculties_has_subjects.sql
+++ university-database/src/main/resources/sql/data/init-faculties_has_subjects.sql
@@ -4,5 +4,12 @@
 USE [university]
 GO
 
+DECLARE @facultyId BIGINT
+DECLARE @subjectId BIGINT
+
+SELECT @facultyId = [id] FROM [faculties] WITH(NOLOCK) WHERE [default_title] = N'Биологический факультет'
+
+INSERT INTO [faculties_has_subjects] ([faculty_id],[subject_id])
+
 -- FIXME: implement
 -- #init-table: faculties_has_subjects  <<< END
