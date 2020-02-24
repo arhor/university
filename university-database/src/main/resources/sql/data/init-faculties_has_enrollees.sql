e5653741fdf415cc6d4b349535a88d7fd719e93a
Maksim Buryshynets <Maksim_Buryshynets@epam.com>
2020-02-24 18:03:40 +0300

    enrollees_has_subjects init in progress;

--- university-database/src/main/resources/sql/data/init-faculties_has_enrollees.sql
+++ university-database/src/main/resources/sql/data/init-faculties_has_enrollees.sql
@@ -1,6 +1,45 @@
--- #dependencies: [faculties, enrollees, faculties_has_enrollees]
+<?xml version="1.0" encoding="UTF-8"?>
+<module xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+        xsi:schemaLocation="http://www.arhor.by/sql-module ./../sql-module.xsd"
+        xmlns="http://www.arhor.by/sql-module"
+        name="init-faculties_has_enrollees">
 
--- #init-table: faculties_has_enrollees  >>> START
+    <dependencies>
+        <dependency name="faculties"/>
+        <dependency name="enrollees"/>
+        <dependency name="faculties_has_enrollees"/>
+    </dependencies>
+
+    <queries context="university">
+        <insert>
+            <![CDATA[
+            DECLARE @totalEnrollees INT = (SELECT COUNT(*) FROM enrollees WITH(NOLOCK))
+
+            DECLARE @counter INT = 0
+            WHILE (@counter < @totalEnrollees)
+            BEGIN
+                DECLARE @enrolleeId BIGINT = (
+                    SELECT e.id
+                    FROM enrollees e WITH(NOLOCK)
+                    ORDER BY e.id ASC
+                    OFFSET @counter ROWS
+                    FETCH NEXT 1 ROWS ONLY
+                );
+
+            	WITH vEnrolleeSubjects (id)
+            	AS
+            	(
+            		SELECT es.subject_id
+            		FROM enrollees_has_subjects es WITH(NOLOCK)
+            		WHERE es.enrollee_id = @enrolleeId
+            	)
 
 
--- #init-table: faculties_has_enrollees  <<< END
+
+            	SET @counter = @counter + 1
+            END
+            ]]>
+        </insert>
+    </queries>
+
+</module>
